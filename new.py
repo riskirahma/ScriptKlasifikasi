c0123f5d23239c268f75b687b99a29371ea5445b
Iky <ramadhanriski0811@gmail.com>
2025-07-23 22:02:57 +0700

    Edit kode biar sama dengan versi lokal

--- new.py
+++ new.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import tensorflow as tf
 from keras.models import load_model
 from keras.preprocessing import image
 from keras.applications.efficientnet_v2 import preprocess_input
